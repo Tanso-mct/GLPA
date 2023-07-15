e1f8879d33fab9814246c4cfb72735c36bf56186
Tanso <tansomctexp@gmail.com>
2023-07-16 00:27:04 +0900

    クラスカプセルか続き

--- GLPA/main.h
+++ GLPA/main.h
@@ -3,13 +3,7 @@
 
 #include <windows.h>
 #include <tchar.h>
-#include <cmath>
-#include <time.h>
 
 #include "window.h"
-// #include "bmp.h"
-#include "graphic.h"
-#include "userinput.h"
-
 
 #endif // MAIN_H_
