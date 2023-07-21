1ad47df0de2737d9ebe5cc54aa0372f9ed9c23b9
Tanso <tansomctexp@gmail.com>
2023-07-21 16:02:50 +0900

    フローチャート進行

--- GLPA/load.h
+++ GLPA/load.h
@@ -1,1 +1,7 @@
-#pragma once
+#ifndef LOAD_H_
+#define LOAD_H_
+
+#include "graphic.h"
+
+
+#endif LOAD_H_
--- GLPA/window.h
+++ GLPA/window.h
@@ -5,18 +5,23 @@
 #include <tchar.h>
 #include <time.h>
 #include <cmath>
+#include <vector>
 
 #include "graphic.h"
 #include "userInput.h"
 
-//WINDOW SETTINGS
+// WINDOW SETTINGS
 #define WINDOW_WIDTH GetSystemMetrics(SM_CXSCREEN)
 #define WINDOW_HEIGHT GetSystemMetrics(SM_CYSCREEN)
 #define DISPLAY_RESOLUTION 1
 
-//TIMER
+// TIMER
 #define REQUEST_ANIMATION_TIMER 1
 #define FPS_OUTPUT_TIMER 2
+
+// FILE TYPE
+#define FILETYPE_BMP 0
+#define FILETYPE_PNG 1
 
 typedef struct tagWND_STATE
 {
@@ -27,13 +32,12 @@ typedef struct tagWND_STATE
 typedef struct tagWND_BUFFER
 {
     HDC hBufDC;
-    HBITMAP hBufBmp;    
-    BITMAPINFO hBufBmpInfo;      
+    HBITMAP hBufBmp;
+    BITMAPINFO hBufBmpInfo;
     LPDWORD lpPixel = (LPDWORD)HeapAlloc(
         GetProcessHeap(),
         HEAP_ZERO_MEMORY,
-        WINDOW_WIDTH*WINDOW_HEIGHT*4
-    );
+        WINDOW_WIDTH *WINDOW_HEIGHT * 4);
 } WND_BUF;
 
 typedef struct tagFPS
@@ -45,64 +49,82 @@ typedef struct tagFPS
     long double fps;
 } WND_FPS;
 
+typedef struct tagRGBA
+{
+    int r;
+    int g;
+    int b;
+    int a;
+} RGBA;
+
+typedef struct tagIMAGE
+{
+    int width;
+    int height;
+    int colorDepth;
+    int compType;
+    std::vector<RGBA> data;
+    int format;
+} IMAGE;
+
 class WNDMAIN
 {
-    public :
-        // Variable for storing arguments of WinMain function
-        int nCmdShow;
-        HINSTANCE hInstance;
+public:
+    // Variable for storing arguments of WinMain function
+    int nCmdShow;
+    HINSTANCE hInstance;
 
-        // Class Registration
-        WNDCLASSEX registerClass
-        (
-            UINT cls_style,
-            WNDPROC wnd_proc,
-            int cls_extra,
-            int wnd_extra,
-            HINSTANCE h_instance,
-            LPWSTR load_icon,
-            LPWSTR load_cursor,
-            int background_color,
-            LPCWSTR menu_resources_name,
-            LPCWSTR cls_name,
-            LPWSTR cls_small_icon
-        );
+    // Class Registration
+    WNDCLASSEX registerClass
+    (
+        UINT cls_style,
+        WNDPROC wnd_proc,
+        int cls_extra,
+        int wnd_extra,
+        HINSTANCE h_instance,
+        LPWSTR load_icon,
+        LPWSTR load_cursor,
+        int background_color,
+        LPCWSTR menu_resources_name,
+        LPCWSTR cls_name,
+        LPWSTR cls_small_icon
+    );
 
-        // Check that classes and windows have been created successfully
-        int checkClass(WNDCLASSEX *pt_class);
-        int checkWindow(HWND created_hWnd);
+    // Check that classes and windows have been created successfully
+    int checkClass(WNDCLASSEX *pt_class);
+    int checkWindow(HWND created_hWnd);
 };
 
 class WINDOW
 {
-    public :
-        HWND hWnd;
-        HDC hWndDC;
-        PAINTSTRUCT hPs;
+public:
+    HWND hWnd;
+    HDC hWndDC;
+    PAINTSTRUCT hPs;
 
-        int monitorWidth;
-        int monitorHeight;
-        int displayResolution = 1;
+    int monitorWidth;
+    int monitorHeight;
+    int displayResolution = 1;
 
-        WND_STATE state;
-        WND_BUF buffer;
-        WND_FPS fps;
+    WND_STATE state;
+    WND_BUF buffer;
+    WND_FPS fps;
 };
 
 class WINDOW_LAU : public WINDOW
 {
-    public :
-        int wndWidth = 1200;
-        int wndHeight = 800;
-        static LRESULT CALLBACK wndProc(HWND h_wnd, UINT msg, WPARAM w_param, LPARAM l_param);
+public:
+    int wndWidth = 1200;
+    int wndHeight = 800;
+    static LRESULT CALLBACK wndProc(HWND h_wnd, UINT msg, WPARAM w_param, LPARAM l_param);
 };
 
 class WINDOW_PLAY : public WINDOW
 {
-    public :
-        int wndWidth = WINDOW_WIDTH;
-        int wndHeight = WINDOW_HEIGHT;
-        static LRESULT CALLBACK wndProc(HWND h_wnd, UINT msg, WPARAM w_param, LPARAM l_param);
+public:
+    int wndWidth = WINDOW_WIDTH;
+    int wndHeight = WINDOW_HEIGHT;
+    static LRESULT CALLBACK wndProc(HWND h_wnd, UINT msg, WPARAM w_param, LPARAM l_param);
 };
 
 extern WNDMAIN WndMain;
