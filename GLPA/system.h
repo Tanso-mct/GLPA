c122f8928ac80deb6cc84e450967308c6558edae
Tanso <tansomctexp@gmail.com>
2023-09-08 15:02:08 +0900

    実行状態記録クラス仮完成

--- GLPA/system.h
+++ GLPA/system.h
@@ -1,1 +1,31 @@
-#pragma once
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+typedef struct tagRUN_STATUS
+{
+    bool load = false;
+    bool mainMenu = false;
+    bool escMenu = false;
+    bool play1 = false;
+    bool play2 = false;
+    bool play3 = false;
+} RUN_STATUS;
+
+// RUN_STATUS
+#define RUN_STATUS_LOAD 0
+#define RUN_STATUS_MAINMENU 1
+#define RUN_STATUS_ESCMENU 2
+#define RUN_STATUS_PLAY1 3
+#define RUN_STATUS_PLAY2 4
+#define RUN_STATUS_PLAY3 5
+
+class RUN_STATUS
+{
+public :
+    int status = RUN_STATUS_LOAD;
+};
+
+extern RUN_STATUS mainRunSystem;
+
+
+#endif SYSTEM_H_
