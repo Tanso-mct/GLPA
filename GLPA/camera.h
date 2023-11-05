fbb5fdda7face4a4902da98ff464b7f2310475b9
Tanso <tansomctexp@gmail.com>
2023-11-05 16:58:09 +0900

    [update]read.me更新

--- GLPA/camera.h
+++ GLPA/camera.h
@@ -19,79 +19,30 @@
 #include "cg.h"
 #include "view_volume.h"
 
+/// @brief Has data related to the 3DCG camera.
 class Camera
 {
 public :
-    /**
-     * @fn
-     * To handle vector type variables, it is necessary to update the data array each time it is redrawn.
-     * Therefore, initialize vector variables that need to be updated
-     * Also initializes any variables that need initialization other than vector types
-     * @brief Initialize variables that need to be initialized each time they are redrawn
-     * @details
-     * Initialize the small polygon information structure 
-     * and the source, calculation, and search polygon information structures. 
-     */
+    /// @brief Initialize variables that need to be initialized each time they are redrawn.
     void initialize();
 
-    /**
-     * @fn
-     * Performs camera coordinate transformation for a rectangular area of a mesh, 
-     * inputting and using data for a rectangular area from meshData.
-     * @brief Perform camera coordinate transformation of the rectangular range of the mesh.
-     * @param (mesh_data) The loaded 3D data is saved as a vector type variable for each mesh.
-     * @details The 8 vertices of the rectangular range are obtained from the 3D coordinates of the origin and opposite 
-     * vertex of the mesh at the read stage, and then transformed to camera coordinates, From those vertices, 
-     * origin and opposite are determined again.
-     */
+    /// @brief Perform camera coordinate transformation of the rectangular range of the mesh.
+    /// @param mesh_data A summary of the imported 3D data stored together.
     void coordinateTransRange(std::vector<OBJ_FILE>* mesh_data);
 
-    // Determination of intersection of OBJECT with view volume
-
-    /**
-     * @fn
-     * Determine which meshes are in the view volume from the rectangular extent of the mesh.
-     * @brief Determine which meshes are in the view volume from the rectangular extent of the mesh.
-     * @param (mesh_data) The loaded 3D data is saved as a vector type variable for each mesh.
-     * @details Calculate the angles of the vertices in the rectangular range of the mesh data in the XZ and YZ axes, 
-     * determine whether the mesh is in the view volume based on the angles, and store the data of the mesh in the view 
-     * volume in the small polygon information structure.
-     */
+    /// @brief Determine which meshes are in the view volume from the rectangular extent of the mesh.
+    /// @param mesh_data A summary of the imported 3D data stored together.
     void clipRange(std::vector<OBJ_FILE> mesh_data);
 
-    // Determining whether the face is front or back
-    /**
-     * @fn
-     * The vertices and normal 3D vectors from the data of the small polygon information structure are converted to 
-     * camera coordinates to determine the front and back of polygons.
-     * @brief Performs front-to-back determination of polygons in the view volume.
-     * @param (mesh_data) The loaded 3D data is saved as a vector type variable for each mesh.
-     * @return A variable of type tuple that stores the three vertices of the polygons in the view volume that face the
-     * table and the normal vector of the face.
-     * @details The world 3D vertex coordinates and world polygon normal vector in the small polygon information
-     * structure are converted to camera coordinates. In this case, the normal vectors are normalized, so the position
-     * information remains unchanged. Using these camera 3D vertex coordinates and camera polygon normal vectors, the
-     * camera determines whether the surface is facing the front or not. If the result is facing the surface, the three
-     * vertices of the polygon and the normal vector are stored in a vector type variable and returned as a return
-     * value using tuple. This value is used in the next camera coordinate transformation function.
-     */
+    /// @brief Performs front-to-back determination of polygons in the view volume.
+    /// @param mesh_data A summary of the imported 3D data stored together.
+    /// @return World 3D vertex coordinates and world normal coordinates of the polygons to be transformed into camera
+    /// coordinates.
     std::tuple<std::vector<VECTOR3D>, std::vector<VECTOR3D>> polyBilateralJudge(std::vector<OBJ_FILE> mesh_data);
 
-    // Coordinate transformation of the vertices of the surface to be drawn
-    /**
-     * @fn
-     * The world 3D vertex coordinates of the source polygon information structure and the camera coordinate
-     * transformation of the world normal vector are performed using the return value of the polyBilateralJudge 
-     * function.
-     * @brief Performs camera coordinate transformation of the vertices and normal vectors of the source polygon 
-     * information structure.
-     * @param (mesh_data) The loaded 3D data is saved as a vector type variable for each mesh.
-     * @sa polyBilateralJudge()
-     * @details Since the order of the array of vector variables in the return value of the polyBilateralJudge function 
-     * and the order of the array of the source polygon information structure are equal, the return value is camera 
-     * coordinate transformed and the camera coordinate transformed data is input into the source polygon information 
-     * structure in turn by loop processing.
-     */
+    /// @brief Performs camera coordinate transformation of the vertices and normal vectors of the source polygon 
+    /// information structure.
+    /// @param mesh_data 
     void coordinateTrans(std::vector<OBJ_FILE> mesh_data);
 
     bool clipVerticesViewVolume();
--- README.md
+++ README.md
@@ -30,6 +30,17 @@ ascalCase    | Class name              |
 
 ### 3. Coding style
 The following coding style was set up for personal production. Therefore, detailed explanations may be omitted in some cases. Please read with this in mind.
+- Always write the file description at the top of the header file. See below for how to write it.
+
+    ```C++
+    /**
+     * @file file_name.h
+     * @brief Brief Description
+     * @author Name of person who wrote this
+     * @date Date?iyear-month?j
+     */
+    ```
+
 - Use doxygen comments for clarity.
 
 - The maximum number of horizontal characters on a page is 120. If you use vscode, it is recommended to add the following code to setting.json for easier coding.     
