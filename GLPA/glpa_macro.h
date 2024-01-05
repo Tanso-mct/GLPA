9c3b29fe4d373f7c6bf551e3e7d60af7f00eca52
Tanso <tansomctexp@gmail.com>
2024-01-05 19:08:15 +0900

    [update]ファイル移行の開始。

--- GLPA/glpa_macro.h
+++ GLPA/glpa_macro.h
@@ -1,10 +1,11 @@
 /**
  * @file glpa_macro.h
- * @brief A list of macros used in glpa. 
- * It prevents errors due to mutual inclusion and at the same time allows you to check the macros used in glpa.
+ * @brief
+ * 日本語 : GLPAで使用している構造体やマクロをまとめたもの。このファイルはGLPAのいずれかのファイルもインクルードしない。
+ * English : A collection of structures and macros used in glpa. This file does not include any of the glpa files.
  * @author Tanso
- * @date Date?iyear-month?j
- */
+ * @date 2024-1
+*/
 
 
 #ifndef GLPA_MACRO_CG_H_
@@ -12,12 +13,23 @@
 
 #include <vector>
 
+
+/**********************************************************************************************************************
+ * 日本語 : 数学に関連する構造体やマクロを以下に記述。
+ * English : Structures and macros related to mathematics are described below.
+**********************************************************************************************************************/
+
+
+/********************************************************************************
+ * 日本語 : ベクトルに関連するものらを記述。
+ * English : Describes those related to vectors.
+********************************************************************************/
+
 typedef struct tagVECTOR2D
 {
     double x;
     double y;
 } VECTOR2D;
-
 
 typedef struct tagVECTOR3D
 {
@@ -27,12 +39,19 @@ typedef struct tagVECTOR3D
 } VECTOR3D;
 
 
+
 typedef struct tagNUMCOMB3
 {
     int num1; // number 1
     int num2; // number 2
     int num3; // number 3
 } NUMCOMB3;
+
+
+/**********************************************************************************************************************
+ * 日本語 : メッシュデータから読み込む情報を格納するために使用するものらを記述。
+ * English : Describes what is used to store information to be read from the mesh data.
+**********************************************************************************************************************/
 
 
 typedef struct tagVERTEX
