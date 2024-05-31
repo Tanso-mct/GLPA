5d5b6eb57226996c5d800f91ae5e817cdab4bace
Tanso <tansomctexp@gmail.com>
2024-05-31 23:43:19 +0900

    [update]シーン関係の移行開始。

--- GLPA/include/SceneObject.h
+++ GLPA/include/SceneObject.h
@@ -1,5 +1,16 @@
-#pragma once
+#ifndef GLPA_SCENE_OBJECT_H_
+#define GLPA_SCENE_OBJECT_H_
+
+namespace Glpa{
+
 class SceneObject
 {
+private :
+
+public :
+    
 };
 
+}
+
+#endif GLPA_SCENE_OBJECT_H_
--- GLPA/src/SceneObject.cpp
+++ GLPA/src/SceneObject.cpp
@@ -1,1 +1,2 @@
 #include "SceneObject.h"
+
