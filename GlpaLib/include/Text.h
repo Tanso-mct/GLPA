20037eea7de13c15604d283629ac5fffabdce1b2
Tanso <tansomctexp@gmail.com>
2024-06-30 23:49:49 +0900

    [continue]Textクラスの作成進行。

--- GlpaLib/include/Text.h
+++ GlpaLib/include/Text.h
@@ -1,9 +1,33 @@
 #ifndef GLPA_TEXT_H_
 #define GLPA_TEXT_H_
 
-class Text
+#include "SceneObject.h"
+
+#include <d2d1.h>
+#include <dwrite.h>
+#pragma comment(lib, "d2d1")
+
+namespace Glpa
 {
+
+class Text : public Glpa::SceneObject
+{
+private :
+    bool visible = true;
+    Glpa::Vec2d pos;
+
+    IDWriteFactory* pDWriteFactory = nullptr;
+    IDWriteTextFormat* pTextFormat = nullptr;
+
+public :
+    Text();
+    ~Text() override;
+
+    void load() override;
+    void release() override;
 };
+
+}
 
 
 #endif GLPA_TEXT_H_
